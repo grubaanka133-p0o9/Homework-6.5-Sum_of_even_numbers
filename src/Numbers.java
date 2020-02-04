4668938e684f2053019b0c191a05ae239b807c3f
Anna Biga <aniabiga040896@gmail.com>
2020-02-04 01:50:23 +0100

    sum of even numbers

---
+++ src/Numbers.java
@@ -0,0 +1,17 @@
+public class Numbers {
+    public static void main(String[] args) {
+        int a = 0;
+        int sum = 0;
+        while(a<=100){
+            System.out.print(a + ", " );
+            sum = sum + a;
+                a++;
+                a++;
+
+            }
+
+        System.out.println("\nSuma wszystkich liczb parzystych od 0 do 100: " + sum);
+        }
+
+    }
+
